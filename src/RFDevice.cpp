6c7e74cc62ffa72052fd08a9c20c73936d259c19
Jozef Sovcik <jozef@sovcik.net>
2021-11-28 13:30:03 +0100

    fixing output

--- src/RFDevice.cpp
+++ src/RFDevice.cpp
@@ -46,7 +46,6 @@ void RFSensorTemp:: update(RFSensorPayload& payload){
 
     // update Homie property
     if (Homie.isConnected()) homie->setProperty(idStr).send(String(temp));
-    CONSOLE(PSTR("%d Sensor-Temp id=0x%X temp=%.2f\n"),millis(),id,temp);
     _logger.logf_P(LOG_NOTICE,PSTR("Sensor-Temp id=0x%X value=%.2f"),id,temp);
 }
 
@@ -62,7 +61,6 @@ void RFSensorContact::update(RFSensorPayload& payload){
 
     // update Homie property
     if (Homie.isConnected()) homie->setProperty(idStr).send(open?"false":"true");
-    CONSOLE(PSTR("%d Sensor-Contact id=0x%X status=%d\n"),millis(),id,!open);
     _logger.logf_P(LOG_NOTICE,PSTR("Sensor-Contact id=0x%X value=%d"),id,!open);
 }
 
@@ -77,6 +75,5 @@ void RFSensorHumidity:: update(RFSensorPayload& payload){
         
     // update Homie property
     if (Homie.isConnected()) homie->setProperty(idStr).send(String(hum));
-    CONSOLE(PSTR("%d Sensor-Hum id=0x%X rh=%.2f\n"),millis(),id,hum);
     _logger.logf_P(LOG_NOTICE,PSTR("Sensor-Humidity id=0x%X value=%.2f"),id,hum);
 }
